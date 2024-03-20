b1aed48ad3e52da28539bd4cfe3cc343aaf34c90
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-20 17:03:09 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  access_key = "AKIA5ZBREE6G2DHCSTHJ"
+  secret_key = "hI28/04mO4OrfhKouoqec/XpoQpnApxKyWRevooR"
+  region = "ap-northeast-2"
+}
 
+module "vpc" {
+  source  = "app.terraform.io/example-org-3f1fdf/vpc/aws"
+  version = "1.0.0"
+  # insert required variables here
+
+  vpc_cidr = "10.5.0.0/16"
+  vpc_name = "module-test-vpc"
+}
