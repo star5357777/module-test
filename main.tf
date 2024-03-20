788ff8bac734ed40cb4c02db5f1ddfca155d28dd
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-20 17:22:52 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,11 @@ module "vpc" {
   vpc_cidr = "10.5.0.0/16"
   vpc_name = "module-test-vpc"
 }
+
+module "subnet" {
+  source  = "app.terraform.io/example-org-3f1fdf/subnet/aws"
+  version = "1.0.0"
+  # insert required variables here
+  vpc_id = module.vpc.vpc_id
+  subnet = var.subnet
+}
