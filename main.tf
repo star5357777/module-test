6f7f1ddcc1a7f97032ad4c6149bb8f77c3716cd3
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-22 14:23:55 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,13 @@ module "subnet" {
   vpc_id = module.vpc.vpc_id
   subnet = var.subnet
 }
+
+module "subnets" {
+  source  = "app.terraform.io/example-org-3f1fdf/subnets/aws"
+  version = "1.0.0"
+  # insert required variables here
+  vpc_id = module.vpc.vpc_id
+  subnet = var.subnet
+  create_route_table = var.create_route_table
+  route_table_name = var.route_table_name
+}
