926f156c0a52973d33359d6e58c294eedad62bc7
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-25 14:59:20 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -43,3 +43,19 @@ variable "sg_name" {
 variable "sg_tag_name" {
   type = string
 }
+
+variable "sg_id" {
+  type = string
+}
+
+variable "sg_rule" {
+  type = map(object({
+    type         = string
+    description  = string
+    to_port      = number
+    from_port    = number
+    protocol     = string
+    sg_cidr      = optional(string)
+    source_sg_id = optional(string)
+  }))
+}
