a1e8e2ccec211da46e977745a83f560615f5fdf5
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-25 15:12:41 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -55,3 +55,54 @@ variable "sg_rule" {
     source_sg_id = optional(string)
   }))
 }
+
+variable "nic_info" {
+	type = map(object({
+		subnet_id = string
+		private_ip = string
+		sg_id = string
+	}))
+}
+
+variable "ebs_info" {
+	type = map(object({
+		az = string
+		storage_type = string
+		storage_size = number
+		storage_name = string
+		kms_arn = string
+	}))
+}
+
+variable "ec2" {
+	type = map(object({
+		ami            = string
+		az             = string
+		ec2_type       = string
+		key_name       = string
+		script         = optional(string, "")
+		ec2_name       = string
+		os_volume_type = string
+		os_volume_size = number
+		os_volume_name = string
+		encrypted      = bool
+		kms_arn        = string
+	}))
+}
+
+variable "nic_attachment_info" {
+	type = map(object({
+		ec2_id = string
+		nic_id = string
+		nic_index = number
+	}))
+}
+
+variable "ebs_attachment" {
+	type = map(object({
+		force_detach = bool
+		device_name = string
+		ebs_id = string
+		ec2_id = string
+	}))
+}
