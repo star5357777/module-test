85deffef3fe4c0745ef358eb1668da9278059140
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-25 14:51:59 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,12 @@ module "route-association" {
   route_table_id = module.subnets.route_table_id.0
   subnet_id = module.subnets.subnet_id
 }
+
+module "security-group" {
+  source  = "app.terraform.io/Enterprise_Test/security-group/aws"
+  version = "1.0.0"
+  # insert required variables here
+  vpc_id = module.vpc.vpc_id
+  sg_name = var.sg_name
+  sg_tag_name = var.sg_tag_name
+}
