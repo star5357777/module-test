3d5ae038bc4dade9d3e2033bf583f79d4e324c63
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-27 15:56:40 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -56,26 +56,60 @@ variable "sg_rule" {
   }))
 }
 
-variable "ami" {
-  type = string
+variable "nic_info" {
+	type = map(object({
+		subnet_id = any
+		private_ip = string
+		sg_id = string
+		subnet_id_idx = number
+	}))
 }
 
-variable "az" {
-  type = string
+variable "ebs_info" {
+	type = map(object({
+		az = string
+		storage_type = string
+		storage_size = number
+		storage_name = string
+		# kms_arn = string
+	}))
 }
 
-variable "ec2_name" {
-  type = string
+variable "ec2" {
+	type = map(object({
+		ami            = string
+		az             = string
+		ec2_type       = string
+		key_name       = string
+		script         = optional(string, "")
+		ec2_name       = string
+		os_volume_type = string
+		os_volume_size = number
+		os_volume_name = string
+		# encrypted      = bool
+		# kms_arn        = string
+	}))
 }
 
-variable "key_name" {
-  type = string
+variable "nic_attachment_info" {
+	type = map(object({
+		nic_ec2_id = any
+		nic_id = any
+		nic_index = number
+		nic_attachment_ec2_idx = number
+		nic_attachment_nic_idx = number
+	}))
 }
 
-variable "ec2_type" {
-  type = string
+
+variable "ebs_attachment" {
+	type = map(object({
+		force_detach = bool
+		device_name = string
+		ebs_id = any
+		ebs_ec2_id = any
+		ebs_attachment_ebs_idx = number
+		ebs_attachment_ec2_idx = number
+	}))
 }
 
-variable "subnet_number" {
-  type = any
-}
