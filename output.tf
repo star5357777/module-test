161f377c3d8da4433904f6b68c0b12f3f67f2321
star5357777 <108916391+star5357777@users.noreply.github.com>
2024-03-27 13:11:09 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "subnet_id" {
-  value = module.subnet.subnet_id
+  value = module.subnets.subnet_id
 }
 
 output "sg_id" {
-  value = module.sg.sg_id
+  value = module.security-group.sg_id
 }
